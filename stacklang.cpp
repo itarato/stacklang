eb23c703de5798967afbf750ae9b2a4f651790fc
Peter Arato <it.arato@gmail.com>
2018-10-29 20:26:33 -0400

    Improving fizzbuzz.

--- stacklang.cpp
+++ stacklang.cpp
@@ -32,9 +32,12 @@ int main(int argc, char* argv[]) {
   //     .interpret();
   // Interpreter("10 dup pop pop pop").interpret();
   Interpreter(
-      "fn fizz 90 90 73 70 4 printstr endfn fn loop if dup 5 % if pop "
+      "fn fizz 90 90 73 70 4 printstr endfn "
+      "fn buzz 90 90 85 66 4 printstr endfn "
+      "fn fizzbuzz 90 90 85 66 90 90 73 70 8 printstr endfn "
+      "fn loop if dup 5 % if pop dup 3 % if pop print else buzz pop endif "
       "else fizz pop "
-      "endif print dec "
+      "endif dec "
       "loop endif endfn 100 loop",
       logger)
       .interpret();
